ad14e53b7a43ccb467f87a8a7ffbf79ec7b3453d
root <root@aurel.top>
2024-06-20 09:57:42 +0700

    [invent-03] Salah Satu Kategori

--- app/Http/Controllers/Api/KategoriController.php
+++ app/Http/Controllers/Api/KategoriController.php
@@ -44,7 +44,14 @@ class KategoriController extends Controller
      */
     public function show(string $id)
     {   
-       
+        $kategori = Kategori::find($id);
+        
+        if(!$kategori){
+            return response()->json(['message' => 'Kategori tidak ditemukan'], 404);
+        }else{
+            $data=array("data"=>$kategori);
+            return response()->json($data);
+        }
     }
     /**
      * Update the specified resource in storage.
